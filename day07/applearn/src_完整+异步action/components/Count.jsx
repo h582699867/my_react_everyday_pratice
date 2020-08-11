488fc55d137a80c70a8af3a5abf2ff53dbba4a9b
黄仕琪 <582699867@qq.com>
2020-08-11 22:19:53 +0800

    更新

--- day07/applearn/src_完整+异步action/components/Count.jsx
+++ day07/applearn/src_完整+异步action/components/Count.jsx
@@ -1,51 +1,46 @@
 import React, { Component } from 'react'
-import { incAction, decAction } from '../redux/count_action.js'
+import { incAction, decAction, incWaitAction } from '../redux/count_action'
 
 export default class Count extends Component {
   // 加
   inc = () => {
     const { value } = this.refs.selectNum
-    // 通知redux加value
     this.props.store.dispatch(incAction(value * 1))
   }
 
   // 减
   dec = () => {
     const { value } = this.refs.selectNum
-    // 通知redux加value
     this.props.store.dispatch(decAction(value * 1))
   }
 
   // 奇数加
-  oddInc = () => {
+  inc = () => {
     if (this.props.store.getState() % 2 !== 0) {
       const { value } = this.refs.selectNum
       this.props.store.dispatch(incAction(value * 1))
     }
   }
 
-  // 延时加
-  waitInc = () => {
-    setTimeout(() => {
-      const { value } = this.refs.selectNum
-      this.props.store.dispatch(incAction(value * 1))
-    }, 500)
+  // 异步加
+  incWait = () => {
+    const { value } = this.refs.selectNum
+    this.props.store.dispatch(incWaitAction(value * 1))
   }
 
   render() {
     return (
       <div>
-        <h2>当前求和为：{this.props.store.getState()}</h2>
+        <h2>最终结果为：{this.props.store.getState()}</h2>
         <select ref="selectNum">
           <option value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
         </select>
-        &nbsp;&nbsp;
-        <button onClick={this.inc}>+</button>&nbsp;&nbsp;
-        <button onClick={this.dec}>-</button>&nbsp;&nbsp;
-        <button onClick={this.oddInc}>奇数+</button>&nbsp;&nbsp;
-        <button onClick={this.waitInc}>延时+</button>&nbsp;&nbsp;
+        <button onClick={this.inc}>+</button>
+        <button onClick={this.dec}>-</button>
+        <button onClick={this.incOdd}>奇数+</button>
+        <button onClick={this.incWait}>延时+</button>
       </div>
     )
   }
--- day07/applearn/src_异步Action/components/Count.jsx
+++ day07/applearn/src_异步Action/components/Count.jsx
@@ -1,29 +1,31 @@
 import React, { Component } from 'react'
-import { incAction, decAction, incWaitAction } from '../redux/count_action'
+import { incAction, decAction, incWaitAction } from '../redux/count_action.js'
 
 export default class Count extends Component {
   // 加
   inc = () => {
     const { value } = this.refs.selectNum
+    // 通知redux加value
     this.props.store.dispatch(incAction(value * 1))
   }
 
   // 减
   dec = () => {
     const { value } = this.refs.selectNum
+    // 通知redux加value
     this.props.store.dispatch(decAction(value * 1))
   }
 
   // 奇数加
-  inc = () => {
+  oddInc = () => {
     if (this.props.store.getState() % 2 !== 0) {
       const { value } = this.refs.selectNum
       this.props.store.dispatch(incAction(value * 1))
     }
   }
 
-  // 异步加
-  incWait = () => {
+  // 延时加
+  waitInc = () => {
     const { value } = this.refs.selectNum
     this.props.store.dispatch(incWaitAction(value * 1))
   }
@@ -31,16 +33,17 @@ export default class Count extends Component {
   render() {
     return (
       <div>
-        <h2>最终结果为：{this.props.store.getState()}</h2>
+        <h2>当前求和为：{this.props.store.getState()}</h2>
         <select ref="selectNum">
           <option value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
         </select>
-        <button onClick={this.inc}>+</button>
-        <button onClick={this.dec}>-</button>
-        <button onClick={this.incOdd}>奇数+</button>
-        <button onClick={this.incWait}>延时+</button>
+        &nbsp;&nbsp;
+        <button onClick={this.inc}>+</button>&nbsp;&nbsp;
+        <button onClick={this.dec}>-</button>&nbsp;&nbsp;
+        <button onClick={this.oddInc}>奇数+</button>&nbsp;&nbsp;
+        <button onClick={this.waitInc}>延时+</button>&nbsp;&nbsp;
       </div>
     )
   }
